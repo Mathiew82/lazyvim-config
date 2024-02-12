d7723fe524a902c205fc1ec167179f2558700114
Mathiew82 <amateo1982@yahoo.es>
2024-02-12 05:42:55 +0100

    Changes in keymaps file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,18 +11,18 @@ vim.keymap.set("n", "<leader>o", "<cmd>:Telescope lsp_document_symbols<cr>")
 -- Delete all buffers but the current one --
 vim.keymap.set("n", "<leader>bq", '<Esc>:%bdelete|edit #|normal`"<cr>')
 
---
-vim.keymap.set("i", "<C-d>", "<C-d>zz")
-vim.keymap.set("i", "<C-u>", "<C-u>zz")
-vim.keymap.set("i", "<C-b>", "<C-o>de")
+-- Move the cursor up or down in the same horizontal position
+vim.keymap.set("i", "<C-d>", "<C-d>zz", { desc = "Move cursor down in the same horizontal position" })
+vim.keymap.set("i", "<C-u>", "<C-u>zz", { desc = "Move cursor up in the same horizontal position" })
 
--- Disable key mappings in insert mode
+-- Disable keymaps ...
+-- Insert mode
 vim.api.nvim_set_keymap("i", "<A-j>", "<Nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<A-k>", "<Nop>", { noremap = true, silent = true })
--- Disable key mappings in normal mode
+-- Normal mode
 vim.api.nvim_set_keymap("n", "<A-j>", "<Nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<A-k>", "<Nop>", { noremap = true, silent = true })
--- Disable key mappings in visual block mode
+-- Visual block mode
 vim.api.nvim_set_keymap("x", "<A-j>", "<Nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "<A-k>", "<Nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "J", "<Nop>", { noremap = true, silent = true })
