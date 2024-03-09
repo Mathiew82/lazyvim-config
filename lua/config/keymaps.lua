41b9ca2fa1ac7432869ead261a977d8405a2595c
Mathiew82 <amateo1982@yahoo.es>
2024-03-09 22:41:38 +0100

    Update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,17 +3,13 @@
 -- Add any additional keymaps here
 
 -- Paste the clipboard register
-vim.keymap.set("n", "<leader>p", '"0p')
-vim.keymap.set("x", "<leader>p", '"0p')
-vim.keymap.set("n", "<leader>P", '"0P')
-vim.keymap.set("x", "<leader>P", '"0P')
+vim.keymap.set("n", "<leader>p", '"0p', { desc = "Paste after cursor" })
+vim.keymap.set("x", "<leader>p", '"0p', { desc = "Paste after cursor" })
+vim.keymap.set("n", "<leader>P", '"0P', { desc = "Paste before cursor" })
+vim.keymap.set("x", "<leader>P", '"0P', { desc = "Paste before cursor" })
 
 -- Delete all buffers but the current one --
 vim.keymap.set("n", "<leader>bq", '<Esc>:%bdelete|edit #|normal`"<cr>')
-
--- Move the cursor up or down in the same horizontal position
-vim.keymap.set("i", "<C-d>", "<C-d>zz", { desc = "Move cursor down in the same horizontal position" })
-vim.keymap.set("i", "<C-u>", "<C-u>zz", { desc = "Move cursor up in the same horizontal position" })
 
 -- Oil keymap
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
