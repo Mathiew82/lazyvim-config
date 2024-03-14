6a3d49a1a1962cecb75a5107393e1d076993eefd
Mathiew82 <amateo1982@yahoo.es>
2024-03-14 04:44:14 +0100

    Added catppuccin theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,9 +13,17 @@ return {
     },
   },
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
     },
   },
 }
